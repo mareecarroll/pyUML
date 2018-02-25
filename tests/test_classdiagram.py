1ccc1d686945266ebb0af6a04dca6a89baed7853
Maree Carroll <maree.carroll@gmail.com>
2018-02-26 09:57:31 +1100

    tests: test Mammal is subclass of Vertibrate

--- tests/test_classdiagram.py
+++ tests/test_classdiagram.py
@@ -13,3 +13,10 @@ def test_get_entities():
     assert len(entities) == 12, "Unexpected number of entities found"
     assert all(isinstance(x, UMLEntity) for x in entities), "Expecting all entities to be of type UMLEntity"
     assert len([x.parent == 'object' for x in entities]), "Expecting only one root class"
+
+def test_mammal_is_vertibrate():
+    """Test mammal is vertibrate."""
+    entities = get_entities(animal)
+    for entity in entities:
+        if entity.subclass == 'Mammal':
+            assert entity.parent == 'Vertibrate', "Expecting Mammal to be subclass of Vertibrate"
